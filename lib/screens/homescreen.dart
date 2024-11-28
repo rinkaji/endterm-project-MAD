788b069c6c6541cebd471843f202e67d3dead00a
mquibol_21ur0788 <mquibol_21ur0788@psu.edu.ph>
2024-11-28 07:13:11 +0000

    trial

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -1,12 +1,49 @@
 import 'package:flutter/material.dart';
 
 class Homescreen extends StatelessWidget {
-  const Homescreen({Key? key}) : super(key: key);
+  Homescreen({Key? key}) : super(key: key);
+
+  List<String> category = [
+    "Block A",
+    "Block B",
+    "Block C",
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      floatingActionButtonLocation: FloatingActionButtonLocation.miniEndFloat,
+      floatingActionButton: FloatingActionButton(
+        shape: CircleBorder(),
+        onPressed: () {},
+        child: Icon(
+          Icons.add,
+          size: 45,
+        ),
+      ),
+      appBar: AppBar(
+        title: Text("Categories"),
+      ),
+      body: ListView.builder(
+        itemCount: category.length,
+        itemBuilder: (BuildContext context, int index) {
+          var item = category[index];
+          return ListTile(
+            title: Text(item),
+            trailing: PopupMenuButton(
+              itemBuilder: (context) => [
+                PopupMenuItem(
+                  child: Text("edit"),
+                ),
+                PopupMenuItem(
+                  onTap: () => print("mamammia"),
+                  child: Text("delete"),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 }
